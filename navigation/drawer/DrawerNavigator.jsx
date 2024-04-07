3f682f1fde629f42570e39feb71eedbbeb3429a4
Himanshu <jain2098@gmail.com>
2024-04-07 02:54:17 -0400

    Added overlay styling as for 3D effects open/close Drawer

--- navigation/drawer/DrawerNavigator.jsx
+++ navigation/drawer/DrawerNavigator.jsx
@@ -6,7 +6,7 @@ import DrawerCustomLayout from './DrawerCustomLayout';
 const Drawer = createDrawerNavigator();
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator drawerContent={props => <DrawerCustomLayout {...props} />}>
+    <Drawer.Navigator drawerContent={props => <DrawerCustomLayout {...props} />} screenOptions={{drawerStyle: {width: 220}, overlayColor: 'transparent',}}>
       <Drawer.Screen name="StackHome" options={{headerShown: false}} component={StackNavigator} />
     </Drawer.Navigator>
   )
