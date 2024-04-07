233eb43a70a74e84c750046d14a450830b6a4392
Himanshu <jain2098@gmail.com>
2024-04-07 02:53:47 -0400

    Added some top padding and Disable Link Redirect

--- navigation/drawer/DrawerCustomLayout.jsx
+++ navigation/drawer/DrawerCustomLayout.jsx
@@ -1,16 +1,20 @@
-import { View, Text, Image, FlatList, TouchableOpacity, Linking, Platform } from "react-native";
+import { View, Text, Image, FlatList, TouchableOpacity, Linking, Platform, ToastAndroid } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 var ios = Platform.OS == "ios";
+
 const DrawerItems = ({navigation}) => {
-  const openURL = (url) => {
-    Linking.openURL(url).catch((err) => console.error('An error occurred', err));
+  const topMargin = ios ? "" : "mt-5";
+  const openURL = (url, e) => {
+    console.log(e)
+    ToastAndroid.showWithGravity('Opening ' + e, ToastAndroid.SHORT, ToastAndroid.CENTER);
+    // Linking.openURL(url).catch((err) => console.error('An error occurred', err));
   }
   return (
     <SafeAreaView className='bg-gray-900' style={{ flex: 1, paddingBottom: 5, gap: 10, }} >
-      <View className='bg-gray-900' style={{ flex:1, flexDirection: 'column', gap:10, maxHeight: 180, justifyContent: "center", alignItems: "center"}} >
+      <View className={`${topMargin}`} style={{ flexDirection: 'column', gap:10, maxHeight: 180, justifyContent: "center", alignItems: "center"}} >
         <Image style={{ width: 100, height: 100, borderRadius: 5, }} source={{ uri: "https://cdn-icons-png.flaticon.com/512/2919/2919906.png", }} />
         <Text style={{ color: "white", fontSize: 20, textAlign: "center", fontWeight: "bold", }} >
           Welcome Back,
@@ -42,10 +46,10 @@ const DrawerItems = ({navigation}) => {
         />
         </View>
         <TouchableOpacity style={{width:"100%", position:'absolute', bottom:0, left:0, flex:1, flexDirection:'row', justifyContent:'space-evenly', alignItems:'center', backgroundColor:"#11001C", paddingVertical:15}}>
-        <Ionicons name='logo-facebook' size={30} color={'#316FF6'} onPress={()=> openURL("https://facebook.com")}/>
-        <Ionicons name='logo-twitter' size={30} color={'#1DA1F2'} onPress={()=> openURL("https://twitter.com")}/>
-        <Ionicons name='logo-whatsapp' size={30} color={'#25D366'} onPress={()=> openURL("https://whatsapp.com")}/>
-        <Ionicons name='logo-instagram' size={30} color={'#E1306C'} onPress={()=> openURL("https://instagram.com")}/>
+        <Ionicons name='logo-facebook' size={30} color={'#316FF6'} onPress={()=> openURL("https://facebook.com", 'Facebook')}/>
+        <Ionicons name='logo-twitter' size={30} color={'#1DA1F2'} onPress={()=> openURL("https://twitter.com", 'Twitter')}/>
+        <Ionicons name='logo-whatsapp' size={30} color={'#25D366'} onPress={()=> openURL("https://whatsapp.com", 'WhatsApp')}/>
+        <Ionicons name='logo-instagram' size={30} color={'#E1306C'} onPress={()=> openURL("https://instagram.com", 'Instagram')}/>
         </TouchableOpacity>
     </SafeAreaView>
   );
