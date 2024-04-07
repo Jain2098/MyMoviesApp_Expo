848c4d189f93ee0ad7fa4e46d205162a05fd7616
Himanshu <jain2098@gmail.com>
2024-04-06 23:00:05 -0400

    Added a Condition that will Reset the Navigation Stack to 0 if pressed homePage, it will use helper function [get the current Screen] to do some validations i.e. if user is on same page and if user is not on the same tab then no need to reset the stack.  [Took 3 hrs]

--- navigation/bottom/BottomNavigator.jsx
+++ navigation/bottom/BottomNavigator.jsx
@@ -4,10 +4,16 @@ import { Cog6ToothIcon, HeartIcon, HomeIcon } from 'react-native-heroicons/solid
 import TvHome from '../../screens/TvHome';
 import DrawerNavigator from '../drawer/DrawerNavigator';
 import { theme } from '../../theme/style';
+import { getCurrentRouteName } from '../../helper/getCurrentRouteName';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavigator() {
+
+
+
+
+
   return (
     <Tab.Navigator
     screenOptions={{
@@ -28,16 +34,45 @@ export default function BottomNavigator() {
             component={DrawerNavigator}
             listeners={({ navigation, route }) => ({
               tabPress: e => {
-                console.log(route)
                 e.preventDefault();
+
                 const state = navigation.getState();
                 const currentTabIndex = state.index;
                 const targetTabIndex = state.routes.findIndex(r => r.name === route.name);
-                console.log('state', state);
-                console.log('currentTabIndex', currentTabIndex);
-                console.log('targetTabIndex', targetTabIndex);
+
+                // console.log('state: \n\n', JSON.stringify(state, null, 2));
+
+                const activeScreen = getCurrentRouteName(state);
+                const noResetScreens = ['HomeScreen', 'Parent'];
+
+                // console.log(state.routes)
                 if (currentTabIndex === targetTabIndex) {
-                  navigation.navigate('HomeScreen');
+                  if(!noResetScreens.includes(activeScreen)){
+                    console.log('Resetting to Parent')
+                    navigation.reset ({
+                      index: 0,
+                      routes: [{ name: 'Parent' }]
+                    })
+                    return;
+                  }
+                  console.log('Already on the same screen')
+
+                  // console.log('\n\nNested State: ', JSON.stringify(nestedState, null, 2))
+                  // console.log('nestedState: ', JSON.stringify(nestedState, null, 2))
+                  // console.log('nestedState.routes: ', JSON.stringify(nestedState.routes, null, 2))
+                  // console.log('nestedState.index: ', nestedState.index)
+                  // console.log('nestedState.routes.length', nestedState.routes.length)
+
+                  // console.log('currentScreenName: ', currentScreenName)
+
+                  // console.log(route.screen)
+                  /*
+                  navigation.reset ({
+                    index: 0,
+                    routes: [{ name: 'HomeScreen' }]
+                  })
+                  */
+                 
                 } else {
                   navigation.navigate(route.name);
                 }
