9d449189cdcb5acf23bef549e221298509798f17
Himanshu <jain2098@gmail.com>
2024-04-08 01:23:23 -0400

    introduced Global_ variables that is being used a lot of places

--- config/config.js
+++ config/config.js
@@ -1,13 +1,24 @@
+import { Dimensions, Platform } from "react-native";
+
+
+
+
+
+
+const { width, height } = Dimensions.get("window");
+export const Global_width = width;
+export const Global_height = height;
+export const Global_ios = Platform.OS == "ios";
+export const Global_topMargin = Global_ios ? "" : "mt-3";
+
 export const fallbackImg = require("../assets/ImageNotFound.jpg");
 
 export const fallbackPersonImage = (gender) => {
   const images = {
     1: require('../assets/girl-icon.png'),
     2: require('../assets/boy-icon.png'),
-    // Default case
     default: require('../assets/anonymous.png') 
   };
-
   return images[gender] || images.default;
 }
 
