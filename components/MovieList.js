878b9fce3beab0ba76847be8366f6603ca2abdf4
Himanshu <jain2098@gmail.com>
2024-04-08 01:22:38 -0400

    major: Added OnPress at seeAll that displays as Vertical List via AchievedScreenPosts

--- components/MovieList.js
+++ components/MovieList.js
@@ -4,9 +4,7 @@ import { styles } from "../theme/style";
 import { useNavigation } from "@react-navigation/native";
 import { img500 } from "../api/api";
 import { CustomImage } from "../helper/CustomImage";
-import { fallbackImg } from "../config/config";
-
-var { height, width } = Dimensions.get("window");
+import { Global_height, Global_width, fallbackImg } from "../config/config";
 
 export default function MovieList({ title, data, hideSeeAll = false }) {
 
@@ -20,15 +18,16 @@ export default function MovieList({ title, data, hideSeeAll = false }) {
         key={index}
         onPress={() => navigation.push("Movie", item)}
       >
-          <View className='space-y-1 mr-4 relative overflow-hidden mx-3' style={{ width: width * 0.33 }}>
+          <View className='space-y-1 mr-4 relative overflow-hidden mx-3' style={{ width: Global_width * 0.33 }}>
             <CustomImage
               initialSource={img500(item.poster_path)} 
               fallbackImage={fallbackImg}
-              style={{ width: width * 0.33, height: height * 0.25 }}
+              type='MOVIE CAROUSEL'
+              style={{ width: Global_width * 0.32, height: Global_height * 0.25 }}
             />
             <Text className='text-neutral-300 ml-1 text-center'>{movieTitle.length > 14 ? movieTitle.slice(0, 14) + "..." : movieTitle}</Text>
             {item.vote_average > 0 && (
-              <View className='absolute top-0 left-0 px-2 py-2 rounded-br-3xl bg-red-900/90'>
+              <View className='absolute origin-top-left top-0 left-0 p-2 rounded-br-3xl bg-red-900/90'>
                 <Text className='text-xs font-bold text-center text-white'>
                   {parseFloat(item.vote_average.toFixed(1))}
                 </Text>
@@ -44,7 +43,9 @@ export default function MovieList({ title, data, hideSeeAll = false }) {
       <View className='mx-4 flex-row justify-between items-center'>
         <Text className='text-white text-2xl'>{title}</Text>
         {!hideSeeAll && (
-          <TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => navigation.navigate("VerticalDataScreen", { data, title })}
+          >
             <Text style={styles.text} className='text-lg'>
               See All
             </Text>
