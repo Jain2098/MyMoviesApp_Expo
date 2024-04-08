2d89e06b84d0996d7817bb9916a1574438fea5db
Himanshu <jain2098@gmail.com>
2024-04-08 01:20:41 -0400

    Type argument added for CustomImage logs

--- components/Cast.jsx
+++ components/Cast.jsx
@@ -20,7 +20,7 @@ export default function Cast({cast, navigation}) {
           return (
             <TouchableOpacity key={index} className="mr-4 items-center" onPress={()=> navigation.push('Person', person)}>
               <View className="overflow-hidden rounded-full h-24 w-20 items-center">
-                <CustomImage initialSource={poster} fallbackImage={fallback} contentFit='cover' className='rounded duration-200 w-20 h-24' />
+                <CustomImage initialSource={poster} fallbackImage={fallback} contentFit='cover' type='CAST' className='rounded duration-200 w-20 h-24' />
               </View> 
               <Text className="text-white text-xs mt-1">{person?.character?.length>10 ? person?.character?.slice(0, 10):person?.character}</Text>
               <Text className="text-neutral-500 text-xs mt-1">{person?.name?.length>10? person?.name?.slice(0,10):person?.name}</Text>
