5f0583bb9c177e95a12b615a9f06b7f3d5723a4f
Himanshu <jain2098@gmail.com>
2024-04-09 20:34:57 -0400

    workStarted to display list of favorite items.

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import {addFavorites, getFavorites, removeFavorites} from "../helper/favouriteManager";
 
 export default function FavoriteScreen() {
+  const [favoritesMovies, setFavoritesMovies] = React.useState([]);
+  React.useEffect(() => {
+    const fetchFavorites = async () => {
+      const favorites = await getFavorites('my_favorite_movies');
+      setFavoritesMovies(favorites);
+    }
+    fetchFavorites();
+  }, []);
   return (
-    <View>
-      <Text>FavoriteScreen</Text>
+    <View className='flex-1 bg-neutral-900 justify-center items-center'>
+      {favoritesMovies.map((item, index) => {
+        return (
+          <View key={index} className='flex-row items-center justify-between w-full px-4 py-2 bg-neutral-800 border-b border-neutral-700'>
+            <Text className='text-white'>{item}</Text>
+            <View className='flex-row items-center'>
+              <Text className='text-white'>Remove</Text>
+            </View>
+          </View>
+        )
+
+      })}
     </View>
   )
 }
