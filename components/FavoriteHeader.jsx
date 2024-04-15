76a7fd69f8e614bc9b18aafcf94380969b889dd9
Himanshu <jain2098@gmail.com>
2024-04-14 21:38:08 -0400

    File Formatted

--- components/FavoriteHeader.jsx
+++ components/FavoriteHeader.jsx
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const FavoriteHeader = () => {
   const [imageLoaded, setImageLoaded] = useState(true);
 
   return (
-    <View style={styles.container}>
+    <View className='w-screen' style={styles.container}>
       {imageLoaded ? (
         <Image
-          source={{ uri: 'https://new3.imgpress.xyz/images/2024/04/13/Heart-On-Fire.webp' }}
+          source={{ uri: "https://new3.imgpress.xyz/images/2024/04/13/Heart-On-Fire.webp" }}
           style={styles.imageStyle}
           onError={() => setImageLoaded(false)}
         />
       ) : (
-        <Text className={`text-3xl text-white text-center font-semibold`}>❤️</Text>
+        <Text className={`text-3xl text-white text-center font-semibold`}>❤️ </Text>
       )}
-      <Text className={`text-3xl text-white text-center font-semibold`}> Collections</Text>
+
+        <Text className={`text-3xl text-white text-center font-semibold`}> My Collections</Text>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row', // aligns image and text in a single line
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row", // aligns image and text in a single line
+    justifyContent: "center",
+    alignItems: "center",
   },
   textStyle: {
     fontSize: 24,
-    color: 'white',
-    textAlign: 'center',
-    fontWeight: 'bold',
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
   },
   imageStyle: {
     width: 50, // width of the image
     height: 50, // height of the image
-    marginRight: 5, // space between the image and the text
-  }
+    marginRight: 0, // space between the image and the text
+  },
 });
 
 export default FavoriteHeader;
