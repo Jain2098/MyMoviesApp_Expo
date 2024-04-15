ef3bc91611a31c7b4462f68708738e85604a3d92
Himanshu <jain2098@gmail.com>
2024-04-14 21:44:17 -0400

    added support for 'tv' favorites i.e. 'my_favorite_tv'

--- App.js
+++ App.js
@@ -7,25 +7,30 @@ import { getFavorites, removeFavorites, addFavorites, removeAllFavorites } from 
 export default function App() {
   const [favoriteMovies, setFavoritesMovies] = React.useState([]);
   const [favoritePeople, setFavoritesPeople] = React.useState([]);
+  const [favoriteTv, setFavoritesTv] = React.useState([]);
 
   React.useEffect(() => {
     const fetchFavorites = async () => {
       const favoriteMovies_ls = await getFavorites("my_favorite_movies");
       const favoritePeople_ls = await getFavorites("my_favorite_persons");
+      const favoriteTv_ls = await getFavorites("my_favorite_tv");
       setFavoritesMovies(favoriteMovies_ls);
       setFavoritesPeople(favoritePeople_ls);
+      setFavoritesTv(favoriteTv_ls);
     };
     fetchFavorites();
   }, []);
 
-  const removeFavorite = async (id, type) => {
+  const removeFavoriteHandler = async (id, type) => {
     await removeFavorites(id, type);
     
     if (type === "my_favorite_movies") {
       setFavoritesMovies([...favoriteMovies.filter((item) => item !== id)]);
     } else if (type === "my_favorite_persons") {
-      console.log("type: ", type)
+      // console.log("type: ", type)
       setFavoritesPeople([...favoritePeople.filter((item) => item !== id)]);
+    } else if (type === "my_favorite_tv") {
+      setFavoritesTv([...favoriteTv.filter((item) => item !== id)]);
     }
   };
 
@@ -37,16 +42,33 @@ export default function App() {
       setFavoritesMovies((prevFavorites) => [...prevFavorites, id]);
     } else if (type === "my_favorite_persons") {
       setFavoritesPeople((prevFavorites) => [...prevFavorites, id]);
+    } else if (type === "my_favorite_tv") {
+      setFavoritesTv((prevFavorites) => [...prevFavorites, id]);
     }
   };
+
+  const removeAllFavoritesHandler = async (type) => {
+    await removeAllFavorites(type);
+    if (type === "my_favorite_movies") {
+      setFavoritesMovies([]);
+    } else if (type === "my_favorite_persons") {
+      setFavoritesPeople([]);
+    } else if (type === "my_favorite_tv") {
+      setFavoritesTv([]);
+    }
+  };
+
+
 
   return (
     <MoviesFavoriteList_Context.Provider
       value={{
         favoriteMovies: favoriteMovies,
         favoritePeople: favoritePeople,
-        removeFavorite: removeFavorite,
+        favoriteTv: favoriteTv,
+        removeFavorite: removeFavoriteHandler,
         addFavoriteViaMain: addFavoriteViaMain,
+        removeAllFavorites: removeAllFavoritesHandler
       }}>
       <View style={{ flex: 1 }}>
         <AppNavigator />
