8612393aaecd992a7ba3c6e50d81ffda6b7de78d
Himanshu <jain2098@gmail.com>
2024-04-14 21:41:53 -0400

    Rendered "tv" Items/Components and minor other things

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,49 +1,110 @@
-import { View, Text, TouchableOpacity, Button, FlatList, ScrollView } from "react-native";
-import React, { useContext, useEffect } from "react";
+import { View, Text, TouchableOpacity, Button, FlatList, ScrollView, Animated, Alert } from "react-native";
+import React, { useContext, useEffect, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import FavoriteItemViewer from "../components/FavoriteItemViewer";
 import { MoviesFavoriteList_Context } from "../store/favoriteItems_store";
 import FavoriteHeader from "../components/FavoriteHeader";
+import { TrashIcon} from "react-native-heroicons/solid";
 
 export default function FavoriteScreen() {
-  const { favoriteMovies, favoritePeople } = useContext(MoviesFavoriteList_Context);
+  const { favoriteMovies, favoritePeople, favoriteTv, removeAllFavorites } = useContext(MoviesFavoriteList_Context);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1500,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
 
   const [moviesSet, setMoviesSet] = React.useState([]);
+  const [tvSet, setTvSet] = React.useState([]);
   const [peopleSet, setPeopleSet] = React.useState([]);
 
   useEffect(() => {
     setMoviesSet([...new Set(favoriteMovies)]);
     setPeopleSet([...new Set(favoritePeople)]);
-    console.log("FavoriteScreen.js: ", favoriteMovies, favoritePeople);
-    console.log("FavoriteScreen.js: ", moviesSet, peopleSet);
-  }, [favoriteMovies, favoritePeople]);
+    setTvSet([...new Set(favoriteTv)]);
+    // console.log("FavoriteScreen.js : Movie: ", favoriteMovies, "\t", favoritePeople);
+    // console.log("FavoriteScreen.js : People ", favoritePeople, "\t", peopleSet);
+    // console.log("FavoriteScreen.js : TV ", favoriteTv, "\t", tvSet);
+  }, [favoriteMovies, favoritePeople, favoriteTv]);
+
+  const deleteAllFavorites = (type) => {
+    Alert.alert("Delete All Favorites", "Are you sure you want to delete all favorites?", [
+      {
+        text: "Cancel",
+        onPress: async () => {
+          console.log("Cancelling: ", type)
+          // await removeAllFavorites(type)
+        },
+      },
+      {
+        text: "Delete",
+        onPress: async () => {
+          console.log("Delete All Favorites: ", type)
+          // await removeAllFavorites(type)
+        },
+      },
+    ]
+    );
+    // await removeAllFavorites(type);
+  }
 
   return (
-    <SafeAreaView className={`flex-1 bg-neutral-800`}>
-      <ScrollView className='py-3'>
+    <SafeAreaView className={`flex-1 bg-neutral-800 `}>
+      <ScrollView className='pt-3 '>
         {/* <Text className={`text-3xl text-white text-center py-5 font-semibold`}>❤️ Collections</Text> */}
         <FavoriteHeader />
         {moviesSet?.length === 0 && peopleSet?.length === 0 && <NoFavorite />}
         {moviesSet?.length > 0 && (
           <>
-            <Text className={`text-2xl text-white text-start font-semibold px-3 pt-3 pb-2`}>Movies</Text>
-            <View className={`flex-row justify-start flex-wrap`}>
+            <View className="flex-row justify-between items-center px-2">
+            <Text className={`text-2xl text-white text-start font-semibold pt-4 pb-2`}>Movies</Text>
+            <TouchableOpacity onPress={() => deleteAllFavorites('my_favorite_movies')}>
+              <TrashIcon className={`w-6 h-6 text-white`} color={'red'} size={25}/>
+            </TouchableOpacity>
+            </View>
+            <Animated.View className={`flex-row justify-start flex-wrap`} style={{opacity: fadeAnim}}>
               {moviesSet?.map((item, index) => {
-                console.log("Movies ITEM", item);
+                // console.log("Movies ITEM", item);
                 return <FavoriteItemViewer key={index} id={item} type={"movie"} />;
               })}
-            </View>
+            </Animated.View>
           </>
         )}
+
+        {tvSet?.length > 0 && (
+          <>
+          <View className="flex-row justify-between items-center px-2">
+            <Text className={`text-2xl text-white text-start font-semibold pt-4 pb-2`}>TV Shows</Text>
+            <TouchableOpacity onPress={() => deleteAllFavorites('my_favorite_tv')}>
+              <TrashIcon className={`w-6 h-6 text-white`} color={'red'} size={25}/>
+            </TouchableOpacity>
+            </View>
+            <Animated.View className={`flex-row justify-start flex-wrap`} style={{opacity: fadeAnim}}>
+              {tvSet?.map((item, index) => {
+                // console.log("TV ITEM", item);
+                return <FavoriteItemViewer key={index} id={item} type={"tv"} />;
+              })}
+            </Animated.View>
+          </>
+        )}
+
         {peopleSet?.length > 0 && (
           <>
-            <Text className={`text-2xl text-white text-start font-semibold px-3 pt-6 pb-2`}>Person</Text>
-            <View className={`flex-row justify-center flex-wrap pb-5`}>
+            <View className="flex-row justify-between items-center px-2">
+            <Text className={`text-2xl text-white text-start font-semibold pt-4 pb-2`}>Person</Text>
+            <TouchableOpacity onPress={() => deleteAllFavorites('my_favorite_persons')}>
+              <TrashIcon className={`w-6 h-6 text-white`} color={'red'} size={25}/>
+            </TouchableOpacity>
+            </View>
+            <Animated.View className={`flex-row justify-center flex-wrap m-0 pb-5`} style={{opacity: fadeAnim}}>
               {peopleSet?.map((item, index) => {
-                console.log("Person ITEM", item);
                 return <FavoriteItemViewer key={index} id={item} type={"person"} />;
               })}
-            </View>
+            </Animated.View>
           </>
         )}
       </ScrollView>
