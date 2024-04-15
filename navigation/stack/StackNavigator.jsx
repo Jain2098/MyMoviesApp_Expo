28838aaa8355f2b6774937ba7f15e327df5f7190
Himanshu <jain2098@gmail.com>
2024-04-14 21:41:29 -0400

    formatted the file

--- navigation/stack/StackNavigator.jsx
+++ navigation/stack/StackNavigator.jsx
@@ -1,34 +1,40 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // import HomeScreen from '../../screens/HomeScreen';
-import MovieScreen from '../../screens/MovieScreen';
-import PersonScreen from '../../screens/PersonScreen';
-import SearchScreen from '../../screens/SearchScreen';
-import SplashScreen, { ResetHomeSplashScreen } from '../../screens/SplashScreen';
+import MovieScreen from "../../screens/MovieScreen";
+import PersonScreen from "../../screens/PersonScreen";
+import SearchScreen from "../../screens/SearchScreen";
+import SplashScreen, { ResetHomeSplashScreen } from "../../screens/SplashScreen";
 
-import GenreScreen from '../../screens/GenreScreen';
-import VerticalDataScreen from '../../screens/VerticalDataScreen';
-import TvScreen from '../../screens/TvScreen';
-import FavoriteScreen from '../../screens/FavoriteScreen';
-import DrawerNavigator from '../drawer/DrawerNavigator';
-
-
+import GenreScreen from "../../screens/GenreScreen";
+import VerticalDataScreen from "../../screens/VerticalDataScreen";
+import TvScreen from "../../screens/TvScreen";
+import FavoriteScreen from "../../screens/FavoriteScreen";
+import DrawerNavigator from "../drawer/DrawerNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Splash" options ={{headerShown: false}} component = {SplashScreen} />
-        <Stack.Screen name ="DrawerHome" options={{headerShown: false}} component={DrawerNavigator} />
-        <Stack.Screen name ="Movie" options={{headerShown: false}} component={MovieScreen} />
-        <Stack.Screen name ="Tv" options={{headerShown: false}} component={TvScreen} />
-        <Stack.Screen name="Person" options={{headerShown: false}} component={PersonScreen} />
-        <Stack.Screen name="Search" options={{headerShown: false}} component={SearchScreen} />
-        {/* <Stack.Screen name ="HomeScreen" options={{headerShown: false}} component={HomeScreen} /> */}
-        <Stack.Screen name ="Genre" options={{headerShown: false}} component={GenreScreen} />
-        <Stack.Screen name ="VerticalDataScreen" options={{headerShown: false}} component={VerticalDataScreen} />
-        <Stack.Screen name ="ResetHomeSplashScreen" options={{headerShown: false}} component={ResetHomeSplashScreen} />
-        {/* <Stack.Screen name ="Favorite" options={{headerShown: false}} component={FavoriteScreen} /> */}
-      </Stack.Navigator>
+    <Stack.Navigator
+      // screenOptions={{
+      //   cardStyleInterpolator: ({ current }) => ({
+      //     cardStyle: {
+      //       opacity: current.progress,
+      //     },
+      //   }),
+      // }}
+      >
+      <Stack.Screen name='Splash' options={{ headerShown: false }} component={SplashScreen} />
+      <Stack.Screen name='DrawerHome' options={{ headerShown: false }} component={DrawerNavigator} />
+      <Stack.Screen name='Movie' options={{ headerShown: false }} component={MovieScreen} />
+      <Stack.Screen name='Tv' options={{ headerShown: false }} component={TvScreen} />
+      <Stack.Screen name='Person' options={{ headerShown: false }} component={PersonScreen} />
+      <Stack.Screen name='Search' options={{ headerShown: false }} component={SearchScreen} />
+      {/* <Stack.Screen name ="HomeScreen" options={{headerShown: false}} component={HomeScreen} /> */}
+      <Stack.Screen name='Genre' options={{ headerShown: false }} component={GenreScreen} />
+      <Stack.Screen name='VerticalDataScreen' options={{ headerShown: false }} component={VerticalDataScreen} />
+      <Stack.Screen name='ResetHomeSplashScreen' options={{ headerShown: false }} component={ResetHomeSplashScreen} />
+      {/* <Stack.Screen name ="Favorite" options={{headerShown: false}} component={FavoriteScreen} /> */}
+    </Stack.Navigator>
   );
 }
